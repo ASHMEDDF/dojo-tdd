9e81c8457eef501479a378edd6ecb31092a65aa4
ASHMEDDF <ashmeddiaz5@gmail.com>
2024-10-23 16:59:13 -0500

    Adjust the code base to implement Maven

--- src/main/java/com/jidoka/dojo/tdd/Calculator.java
+++ src/main/java/com/jidoka/dojo/tdd/Calculator.java
@@ -2,8 +2,5 @@ package com.jidoka.dojo.tdd;
 
 public class Calculator {
 
-    public int add(int a, int b) {
-        return a + b;
-    }
 
 }
--- src/test/java/com/jidoka/dojo/tdd/CalculatorTests.java
+++ src/test/java/com/jidoka/dojo/tdd/CalculatorTests.java
@@ -1,29 +1,5 @@
 package com.jidoka.dojo.tdd;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class CalculatorTests {
 
-    @Test
-    public void testZeroPlusZero() {
-        Calculator calculator = new Calculator();
-        int result = calculator.add(0, 0);
-        assertEquals(0, result);
-    }
-
-    @Test
-    public void testAPlusZero() {
-        Calculator calculator = new Calculator();
-        int result = calculator.add(1, 0);
-        assertEquals(1, result);
-    }
-
-    @Test
-    public void testAPlusB() {
-        Calculator calculator = new Calculator();
-        int result = calculator.add(1, 2);
-        assertEquals(3, result);
-    }
 }
